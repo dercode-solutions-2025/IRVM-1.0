c4c1c08a70ce52fb2aa1fde0d81947a33c0e4382
nobody_on_earth <reddeadarthurfan1889@gmail.com>
2026-01-18 18:09:21 -0600

    Update main.py

--- IRVM/main.py
+++ IRVM/main.py
@@ -1,6 +1,18 @@
 from insttohex import *
+import sys
 stack = []
 inst = insttohex()
-if inst[0] == "PUSH":
+math = []
+if inst[0] == "0x00":
+  sys.exit()
+elif inst[0] == "0x01":
   stack.add(inst[1])
-if inst
+elif inst[0] == "0x02":
+  stack.pop(stack[-1])
+elif inst[0] == "0x03":
+  sum(math.append(stack[-2:]))
+  math = []
+elif inst[0] == "0x04":
+  stack.add(stack[-1] - stack[-2])
+elif inst[0] == "0x05":
+  print(stack[-1])
